d59a88f9954e29c7aa8047c7e91fa3456a2d7405
Victor Leandro Coelho <victorleanc@gmail.com>
2025-11-18 18:51:35 -0300

    adição de soft e hard skills

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/victorleanc/js-resume-portfolio/refs/heads/main/data/profile.json';
+  const url = 'http://127.0.0.1:5500/data/profile.json';
   const fetching = await fetch(url)
   return await fetching.json()
 }
