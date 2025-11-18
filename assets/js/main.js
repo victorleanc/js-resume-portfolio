596699dac6b4a0428c3958dc410db24373875674
Victor Leandro Coelho <victorleanc@gmail.com>
2025-11-18 20:35:00 -0300

    alterações no header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo')
-    photo.src = profileData.photo
-    photo.alt = profileData.name
+    const myPhoto = document.getElementById('profile.photo')
+    myPhoto.src = profileData.photo
+    myPhoto.alt = profileData.name
 
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
