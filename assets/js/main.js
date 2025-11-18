46711ceda045f38466987e3a26b67f6580752dda
Victor Leandro Coelho <victorleanc@gmail.com>
2025-11-17 21:50:09 -0300

    alterando interações da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
+
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+}
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
-
+    updateProfileInfo(profileData)
 })()
